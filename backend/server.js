c04c64b341d8f7a842a36ebe5e6b8084b257661f
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-08-02 19:36:01 -0300

    chore: Update express.json() middleware to include a limit for request body size

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const BASE_URL = process.env.BASE_URL;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
