d7cf3d8aee039cc8df57bd56de050c6482ea5f77
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-08-03 20:49:34 -0300

    feat: Update Posts component to use react-query for data fetching

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,13 +3,17 @@ import { useEffect } from "react";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import Post from "./Post";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
   const getPostEndpoint = () => {
     switch (feedType) {
       case "forYou":
         return "/api/posts/all";
       case "following":
         return "/api/posts/following";
+      case "posts":
+        return `/api/posts/user/${username}`;
+        case "likes":
+        return `/api/posts/likes/${userId}`;
       default:
         return "/api/posts/all";
     }
